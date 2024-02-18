e41dd422656da4bb773e12c9107f20dc35641f35
Jesus Abarca <jesus.abarca.g@gmail.com>
2024-02-18 15:59:59 -0600

    Configuración para commpilar statico

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import './main.scss';
+	export const prerender = true;
+
 </script>
 
 <slot />
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -11,7 +10,15 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// Opciones por defecto se muestran aquí. En algunas plataformas,
+			// estas opciones se establecen automáticamente, consulta más abajo
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		  })
 	}
 };
 
